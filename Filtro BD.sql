e7a4d872c4ac66ca6050f7cd1abb75377a6dd57b
Wilmer Alexander Rojas Fuentes <122381752+wilmer7522@users.noreply.github.com>
2024-10-25 12:59:34 -0500

    feat :sparkles: first DataBase

---
+++ Filtro BD.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE migrupot2; -- crear base de datos
+
+USE migrupoT2; -- utilizar mi BBDD
+
+CREATE TABLE VEHICULOS( Vin int not null primary key,
+Año int not null,
+Precio int not null,
+Tipo_Combustible varchar(45) null,
+Tipo_Transmision varchar(45) null,
+Estado_Vehiculo varchar(45) null,
+
+
+);
+
+CREATE TABLE Color (
+describe VEHICULOS;
